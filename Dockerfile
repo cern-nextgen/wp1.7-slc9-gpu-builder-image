525dfb8a554347f0239be9adef49e8bb10de69ca
Oliver Rietmann <oliver.rietmann@cern.ch>
2026-04-17 18:31:16 +0200

    Install to /opt/clang-p2996

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
-FROM registry.cern.ch/alisw/slc9-gpu-builder@sha256:ea3443f9dfbc770e4b4bce0d1a9ecc0b7a7c16e9f76e416b796d170877220820
-COPY install.sh /tmp
-RUN bash /tmp/install.sh && rm -f /tmp/install.sh /root/anaconda-ks.cfg /root/original-ks.cfg
+FROM registry.cern.ch/alisw/slc9-gpu-builder@sha256:ea3443f9dfbc770e4b4bce0d1a9ecc0b7a7c16e9f76e416b796d170877220820 as builder
 
+ENV GIT_DIR=/tmp/clang-p2996
+ENV BUILD_DIR=/tmp/build
+ENV CLANG_INSTALL_DIR=/opt/clang-p2996
+
+COPY install.sh /tmp
+
+RUN bash /tmp/install.sh
+
+
+FROM registry.cern.ch/alisw/slc9-gpu-builder@sha256:ea3443f9dfbc770e4b4bce0d1a9ecc0b7a7c16e9f76e416b796d170877220820
+
+COPY --from=builder /opt/clang-p2996 /opt/clang-p2996
--- install.sh
+++ install.sh
@@ -1,18 +1,34 @@
-#! /bin/bash
+#!/bin/bash
 
-export GIT_DIR=/tmp/clang-p2996
-export BUILD_DIR=/tmp/build
-export INSTALL_DIR=/usr
+# ---- Required environment checks ----
+: "${GIT_DIR:?GIT_DIR not set}"
+: "${BUILD_DIR:?BUILD_DIR not set}"
+: "${CLANG_INSTALL_DIR:?CLANG_INSTALL_DIR not set}"
 
+# ---- Install dependencies ----
 dnf install -y lld
-dnf clean all
 
-git clone --depth 1 --branch p2996 https://github.com/bloomberg/clang-p2996.git ${GIT_DIR}
-cmake -S ${GIT_DIR}/llvm -B ${BUILD_DIR} -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DLLVM_ENABLE_LLD=ON \
-        -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_APPEND_VC_REV=OFF -DLLVM_ENABLE_ASSERTIONS=ON \
-        -DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;lld' -DLLVM_ENABLE_RUNTIMES="libc;libcxx;libcxxabi;libunwind" \
-        -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_DOCS=OFF \
-        -DLLVM_BUILD_BENCHMARKS=OFF -DLIBCXX_INSTALL_MODULES=ON -DLIBCXX_ENABLE_INCOMPLETE_FEATURES=ON
-cmake --build ${BUILD_DIR} --target install -j $(nproc)
-rm -rf ${GIT_DIR} ${BUILD_DIR}
+# ---- Clone LLVM/Clang ----
+git clone --depth 1 --branch p2996 https://github.com/bloomberg/clang-p2996.git "${GIT_DIR}"
+
+# ---- Configure ----
+cmake -S "${GIT_DIR}/llvm" -B "${BUILD_DIR}" \
+  -DCMAKE_C_COMPILER=gcc \
+  -DCMAKE_CXX_COMPILER=g++ \
+  -DLLVM_ENABLE_LLD=ON \
+  -DLLVM_BUILD_LLVM_DYLIB=ON \
+  -DLLVM_LINK_LLVM_DYLIB=ON \
+  -DLLVM_APPEND_VC_REV=OFF \
+  -DLLVM_ENABLE_ASSERTIONS=ON \
+  -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld" \
+  -DLLVM_ENABLE_RUNTIMES="libc;libcxx;libcxxabi;libunwind" \
+  -DCMAKE_INSTALL_PREFIX="${CLANG_INSTALL_DIR}" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLLVM_BUILD_DOCS=OFF \
+  -DLLVM_BUILD_BENCHMARKS=OFF \
+  -DLIBCXX_INSTALL_MODULES=ON \
+  -DLIBCXX_ENABLE_INCOMPLETE_FEATURES=ON
+
+# ---- Build + install ----
+cmake --build "${BUILD_DIR}" --target install -j "$(nproc)"
 
